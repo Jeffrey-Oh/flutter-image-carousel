f4cbaca42c95818cd09743fc9e11e2d738952749
오태현 <jeffrey@kai-i.com>
2022-06-10 17:17:38 +0900

    PageView 위젯 사용해보기

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 인프런 플러터 강의
  - 전자액자 Image Carousel
 
- - [ ] PageView 위젯 사용해보기
+ - [x] PageView 위젯 사용해보기
  - [ ] Timer 사용해보기
  - [ ] PageController 사용해보기
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:image_carousel/screen/home/HomeScreen.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: null,
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
   ));
 }
